cff5d6a69f6e244aa59befff7e1c29efbc15eefd
Tanya <fomina.tatianaaa@yandex.ru>
2018-06-30 23:12:02 +0300

    changed Player's tick method to make sure player object doesnt get beyond the frame

--- src/tdgaming/gameobjects/Player.java
+++ src/tdgaming/gameobjects/Player.java
@@ -10,10 +10,12 @@ import java.awt.image.BufferedImage;
 public class Player extends GameObject {
 
     private BufferedImage player;
+    private Game game;
 
     public Player(double x, double y, Game game) {
         this.x = x;
         this.y = y;
+        this.game = game;
 
         SpriteSheet ss = new SpriteSheet(game.getSpriteSheet());
 
@@ -29,6 +31,15 @@ public class Player extends GameObject {
     public void tick() {
         x += velX;
         y += velY;
+
+        if (x <= -32 / 2)
+            x = game.getWidth();
+        if (y <= -32 / 2)
+            y = game.getHeight();
+        if (x > game.getWidth())
+            x = 0;
+        if (y > game.getHeight())
+            y = 0;
     }
 
 
